3de32eb6cbb60df9e8e3b52d2149e8cc85038702
Nariman Abdullin <nulls.narik@gmail.com>
2023-03-29 18:12:49 +0300

    create a stub for boolean grammar

--- tree-sitter-boolean/grammar.js
+++ tree-sitter-boolean/grammar.js
@@ -1,16 +1,58 @@
+const PREC = {
+    BIT_OR: 0,      // |
+    BIT_XOR: 1,     // ^
+    BIT_AND: 2,     // &
+    EQUALITY: 3,    // ==  !=
+    UNARY: 4,       // !
+};
+
 module.exports = grammar({
     name: 'boolean',
 
     rules: {
-        expression: $ => repeat($.statement),
+        statement: $ => repeat($.expression),
+
+        expression: $ => choice(
+            $.binary_expression,
+            $.unary_expression,
+            $.primary_expression,
+        ),
+
+        binary_expression: $ => choice(
+            ...[
+                ['==', PREC.EQUALITY],
+                ['!=', PREC.EQUALITY],
+                ['&', PREC.BIT_AND],
+                ['|', PREC.BIT_OR],
+                ['^', PREC.BIT_XOR],
+            ].map(([operator, precedence]) =>
+                prec.left(precedence, seq(
+                    field('left', $.expression),
+                    field('operator', operator),
+                    field('right', $.expression)
+                ))
+            )),
+
+        unary_expression: $ => prec.left(PREC.UNARY, seq(
+            field('operator', '!'),
+            field('operand', $.expression)
+        )),
+
+        primary_expression: $ => choice(
+            $._literal,
+            $.identifier,
+        ),
 
         _literal: $ => choice(
             $.true,
             $.false,
         ),
 
+
         true: $ => 'true',
 
         false: $ => 'false',
+
+        identifier: $ => /[\p{L}_$][\p{L}\p{Nd}_$]*/,
     }
 });
